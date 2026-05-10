a64fc372633b1be3b52144ca066438d951e5613c
Sergey <148696269+BrayvenBro@users.noreply.github.com>
2026-05-10 11:13:05 +0200

    addCalendar

--- diploma-application/src/App.js
+++ diploma-application/src/App.js
@@ -1,30 +1,16 @@
 import './App.css';
 import Header from './Components/Header/Header';
 
-<<<<<<< Updated upstream
-function App() {
-  return (
-    <div className="App">
+import CalendarNote from './Components/Calendar/Calendar'
 
-      
-
-   
-     
-
-
-    </div>
-   
-  );
-}
-=======
 const App = () => {
 return(  <div>
   <Header />
-  <Registration />
-  
+
+  <CalendarNote/>
 </div>)
 };
->>>>>>> Stashed changes
+
 
 export default App;
 
